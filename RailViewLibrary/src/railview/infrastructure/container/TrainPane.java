feac6c7d25f8955de23a1d02acd7379fdb7d793d
herrcui <dr.yong.cui@gmail.com>
2015-08-20 17:59:07 +0200

    * first implementation of TrainPane

---
+++ RailViewLibrary/src/railview/infrastructure/container/TrainPane.java
@@ -0,0 +1,37 @@
+package railview.infrastructure.container;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import railapp.simulation.train.AbstractTrainSimulator;
+import railapp.units.Coordinate;
+
+public class TrainPane extends PannablePane {
+	
+	public TrainPane() {
+		this.widthProperty().addListener(observable -> draw());
+		this.heightProperty().addListener(observable -> draw());
+	}
+	
+	private void draw() {
+
+	}
+
+	void setCoordinateMap(Map<AbstractTrainSimulator, List<Coordinate>> map) {
+		this.coordinateMap = map;
+		
+		if (this.coordinateMap != null) {
+			for (Entry<AbstractTrainSimulator, List<Coordinate>> entry : this.coordinateMap.entrySet()) {
+				List<Coordinate> coordinateList = entry.getValue();
+				if (coordinateList != null) {
+					System.out.println(entry.getKey().getTrain().getNumber() + 
+							": X: " + coordinateList.get(0).getX() +
+							": Y: " + coordinateList.get(0).getY());
+				}
+			}
+		}
+	}
+	
+	private Map<AbstractTrainSimulator, List<Coordinate>> coordinateMap;
+}
