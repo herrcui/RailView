6abc4035bc5c73fc38031a91518b050c095942ca
cu <cu@IEV-VWI962.IEVVWI.local>
2015-10-13 16:42:31 +0200

    * information for table

--- RailViewLibrary/src/railview/infrastructure/container/SwarmPane.java
+++ RailViewLibrary/src/railview/infrastructure/container/SwarmPane.java
@@ -47,6 +47,7 @@ public class SwarmPane extends PannablePane {
 			Color color = this.getSwarmColor(swarm);
 			for (AbstractTrainSimulator train : swarm.getTrains(this.currentTime)) {
 				this.drawTrain(train ,color);
+				//System.out.println(this.currentTime.toString() + swarm.getId() + train.getTrain().getNumber());
 			}
 		}
 	}
