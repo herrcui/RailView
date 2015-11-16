16b161d8e0031a1751fbe1084910a3f9d2b2eff2
Katsching <kai.chen@gmx.de>
2015-11-16 10:53:53 +0100

    - updated SimulationView - introduced some Panes for the graphPane

--- RailViewLibrary/src/railview/infrastructure/container/GraphPaneController.java
+++ RailViewLibrary/src/railview/infrastructure/container/GraphPaneController.java
@@ -1,7 +1,19 @@
 package railview.infrastructure.container;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 public class GraphPaneController {
 	
+	@FXML
+	private AnchorPane firstLayer;
+	
+	@FXML
+	private AnchorPane secondLayer;
+	
+	@FXML
+	public void initialize() {
+		secondLayer.setVisible(false);
+		
+	}
 }
--- SimulationView/src/railview/simulation/SimulationController.java
+++ SimulationView/src/railview/simulation/SimulationController.java
@@ -4,10 +4,15 @@ import java.io.IOException;
 import java.net.URL;
 
 import javafx.animation.FadeTransition;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import railapp.infrastructure.service.IInfrastructureServiceUtility;
@@ -16,6 +21,7 @@ import railapp.simulation.events.EventListener;
 import railapp.simulation.train.AbstractTrainSimulator;
 import railapp.units.Duration;
 import railview.controller.framework.AbstractSimulationController;
+import railview.infrastructure.container.GraphPaneController;
 import railview.infrastructure.container.NetworkPaneController;
 
 public class SimulationController extends AbstractSimulationController {
@@ -35,6 +41,9 @@ public class SimulationController extends AbstractSimulationController {
 	private AnchorPane menuPane;
 
 	@FXML
+	private AnchorPane symbolPane;
+	
+	@FXML
 	protected Button startButton;
 
 	@FXML
@@ -42,6 +51,21 @@ public class SimulationController extends AbstractSimulationController {
 
 	@FXML
 	protected Button stopButton;
+	
+	@FXML
+	private Button graphButton;
+
+	@FXML
+	private Button swarmButton;
+	
+	@FXML
+	private Button lockButton;
+	
+	@FXML
+	private Button unlockButton;
+	
+	@FXML
+	private Slider speedBar;
 
 	@FXML
 	public void initialize() {
@@ -50,11 +74,31 @@ public class SimulationController extends AbstractSimulationController {
 			URL location = NetworkPaneController.class
 					.getResource("NetworkPane.fxml");
 			loader.setLocation(location);
-			StackPane networkPane = (StackPane) loader.load();
+			networkPane = (StackPane) loader.load();
 			this.networkPaneController = loader.getController();
-			AnchorPane.setLeftAnchor(networkPane, (this.networkPaneRoot.prefWidth(-1)/2)-(networkPane.prefWidth(-1)/2));
-			AnchorPane.setTopAnchor(networkPane,(this.networkPaneRoot.prefHeight(-1)/2)-(networkPane.prefHeight(-1)/2));
-			this.networkPaneRoot.getChildren().add(networkPane);
+			
+			FXMLLoader graphpaneloader = new FXMLLoader();
+			URL graphpanelocation = GraphPaneController.class
+					.getResource("GraphPane.fxml");
+			graphpaneloader.setLocation(graphpanelocation);
+			graphPane = (AnchorPane) graphpaneloader.load();
+			
+			this.networkPaneRoot.getChildren().addAll(networkPane, graphPane);
+			
+			networkPaneRoot.widthProperty().addListener(new ChangeListener<Number>() {
+			    @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneWidth, Number newSceneWidth) {
+			    	networkPane.setLayoutX((newSceneWidth.doubleValue() / 2)- (networkPane.prefWidth(-1) / 2));
+			    }
+			});
+
+			networkPaneRoot.heightProperty().addListener(new ChangeListener<Number>() {
+			    @Override public void changed(ObservableValue<? extends Number> observableValue, Number oldSceneHeight, Number newSceneHeight) {
+			    	networkPane.setLayoutY((newSceneHeight.doubleValue() / 2)- (networkPane.prefHeight(-1) / 2));
+			    }
+			});
+			
+			graphPane.setVisible(false);
+			symbolPane.setOpacity(0.0);
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -87,34 +131,90 @@ public class SimulationController extends AbstractSimulationController {
 		this.pauseButton.setDisable(true);
 		this.stopButton.setDisable(true);
 	}
-
 	@FXML
-	private void fadeRoot() {
-	                FadeTransition fadeTransition
-	                        = new FadeTransition(javafx.util.Duration.millis(500), menuPane);
-	                fadeTransition.setToValue(0.0);
-	                fadeTransition.play();
+	public void lock(){
+		
+		menuPane.setOnMouseEntered(new EventHandler<MouseEvent>(){
+
+            public void handle(MouseEvent event)
+            {
+            	menuPane.setOpacity(1.0);
+            }
+		});
+		menuPane.setOnMouseExited(new EventHandler<MouseEvent>(){
+
+            public void handle(MouseEvent event)
+            {
+            	menuPane.setOpacity(1.0);
+            }
+		});
+		lockButton.setVisible(false);
+		unlockButton.setVisible(true);
+	}
+	
+	@FXML
+	public void unlock(){
+		
+			menuPane.setOnMouseEntered(new EventHandler<MouseEvent>(){
+
+	            public void handle(MouseEvent event)
+	            {
+			FadeTransition fadeTransition1 = new FadeTransition(
+					javafx.util.Duration.millis(500), menuPane);
+			fadeTransition1.setFromValue(0);
+			fadeTransition1.setToValue(1.0);
+			fadeTransition1.play();
 	            }
+			});
+			
+			menuPane.setOnMouseExited(new EventHandler<MouseEvent>(){
 
-
-	@FXML
-	private void appear() {
-	                FadeTransition fadeTransition
-	                        = new FadeTransition(javafx.util.Duration.millis(500), menuPane);
-	                fadeTransition.setFromValue(0.0);
-	                fadeTransition.setToValue(1.0);
-	                fadeTransition.play();
+	            public void handle(MouseEvent event)
+	            {
+	    			FadeTransition fadeTransition2 = new FadeTransition(
+	    					javafx.util.Duration.millis(500), menuPane);
+	    			fadeTransition2.setFromValue(1.0);
+	    			fadeTransition2.setToValue(0.0);
+	    			fadeTransition2.play();
 	            }
-
+			});
+			
+			unlockButton.setVisible(false);
+			lockButton.setVisible(true);
+			
+			
+}
+	
+	@FXML
+	private void appearSymbolPane() {
+		FadeTransition fadeTransition = new FadeTransition(
+				javafx.util.Duration.millis(500), symbolPane);
+		fadeTransition.setFromValue(0.0);
+		fadeTransition.setToValue(1.0);
+		fadeTransition.play();
+	}
 
 	@FXML
-	private void fadeMenu() {
-	                FadeTransition fadeTransition
-	                        = new FadeTransition(javafx.util.Duration.millis(500), menuPane);
-	                fadeTransition.setFromValue(1.0);
-	                fadeTransition.setToValue(0.0);
-	                fadeTransition.play();
-				}
+	private void fadeSymbolPane() {
+		FadeTransition fadeTransition = new FadeTransition(
+				javafx.util.Duration.millis(500), symbolPane);
+		fadeTransition.setFromValue(1.0);
+		fadeTransition.setToValue(0.0);
+		fadeTransition.play();
+
+	}
+
+	@FXML
+	public void graphButtonClicked() {
+		graphPane.setVisible(true);
+		networkPane.setVisible(false);
+	}
+
+	@FXML
+	public void swarmButtonClicked() {
+		graphPane.setVisible(false);
+		networkPane.setVisible(true);
+	}
 
 	public NetworkPaneController getNetworkPaneController() {
 		return this.networkPaneController;
@@ -168,6 +268,20 @@ public class SimulationController extends AbstractSimulationController {
 
 	@Override
 	protected void setTime(boolean isReplay) {
+		Duration updateInterval = Duration.fromTotalMilliSecond(UIPause);
+		if (simulator.getStatus() != SimulationManager.INACTIVE) {
+			updateInterval = Duration.fromTotalMilliSecond(MAXSpeed * speedBar.getValue()/100);
+			if (speedBar.getValue() == speedBar.getMin()) {
+				updateInterval = Duration.fromTotalMilliSecond(UIPause);
+			}
+
+			if (speedBar.getValue() == speedBar.getMax() &&
+				simulator.getTime() != null &&
+				simulator.getStatus() != SimulationManager.TERMINATED) {
+
+				updateInterval = simulator.getTime().getDifference(updateTime);
+			}
+		}
 		if (isReplay) {
 			this.updateTime = this.updateTime.add(updateInterval);
 		} else {
@@ -182,6 +296,10 @@ public class SimulationController extends AbstractSimulationController {
 		}
 	}
 
+	private StackPane networkPane;
+	private AnchorPane graphPane;
 	private NetworkPaneController networkPaneController;
 	private Duration updateInterval = Duration.fromSecond(60);
+	private int UIPause = 100;
+	private int MAXSpeed = 20000; // 1 : 200
 }
