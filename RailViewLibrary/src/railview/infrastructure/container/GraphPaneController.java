53caa5208c8d4470e5f40d7363b4b39b8c8560e3
cu <cu@IEV-VWI962.IEVVWI.local>
2015-11-23 20:27:51 +0100

    * add data for simulator

--- RailViewLibrary/src/railview/infrastructure/container/GraphPaneController.java
+++ RailViewLibrary/src/railview/infrastructure/container/GraphPaneController.java
@@ -1,5 +1,15 @@
 package railview.infrastructure.container;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+import railapp.simulation.events.toinfrastructure.RequestResourceEvent;
+import railapp.simulation.events.toinfrastructure.RequestResult;
+import railapp.units.Duration;
+import railapp.units.Time;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 
@@ -16,4 +26,48 @@ public class GraphPaneController {
 		secondLayer.setVisible(false);
 		
 	}
+	
+	// for the historgamm, go throught the sorted duration, and get the size:
+	//
+	// (double) entry.getKey().getTotalMilliSecond()/1000
+	// entry.getValue().size()
+	private TreeMap<Duration, List<RequestResourceEvent>> getHistogrammData() {
+		TreeMap<Duration, List<RequestResourceEvent>> result = new TreeMap<Duration, List<RequestResourceEvent>>();
+		double currentLimit = interval.getTotalMilliSecond();
+		List<RequestResourceEvent> list = new ArrayList<RequestResourceEvent>();
+		for (Entry<Duration, List<RequestResourceEvent>> entry : durationMap.entrySet()) {
+			if (entry.getKey().getTotalMilliSecond() <= currentLimit) {
+				list.addAll(entry.getValue());
+			} else {
+				// increase the currentLimit, until currentLimit is bigger than the current duration
+				while (entry.getKey().getTotalMilliSecond() > currentLimit) {
+					result.put(Duration.fromTotalMilliSecond(currentLimit), list);
+					currentLimit += interval.getTotalMilliSecond();
+					list = new ArrayList<RequestResourceEvent>();
+				}
+								
+				list = entry.getValue();
+			}
+		}
+		result.put(Duration.fromTotalMilliSecond(currentLimit), list);
+		return result;
+	}
+	
+	private List<RequestResourceEvent> getAllRequests(
+			TreeMap<Duration, List<RequestResourceEvent>> histogramm, Duration duration) {
+		Entry<Duration, List<RequestResourceEvent>> floorEntry = histogramm.floorEntry(duration);
+		if (floorEntry != null) {
+			return floorEntry.getValue();
+		} else {
+			return null;
+		}
+	}
+	
+	private LinkedHashMap<Time, RequestResult> getResultMap(RequestResourceEvent event) {
+		return event.getResultMap();
+	}
+	
+	private TreeMap<Duration, List<RequestResourceEvent>> durationMap;
+	
+	private Duration interval = Duration.fromSecond(120);
 }
