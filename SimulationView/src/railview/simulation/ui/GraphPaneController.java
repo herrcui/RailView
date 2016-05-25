0e42f494931ef92365ce68a3a45e630702b76686
herrcui <dr.yong.cui@gmail.com>
2016-05-25 15:17:01 +0200

    * using draggable chart

--- SimulationView/src/railview/simulation/ui/GraphPaneController.java
+++ SimulationView/src/railview/simulation/ui/GraphPaneController.java
@@ -26,6 +26,7 @@ import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Side;
+import javafx.scene.Node;
 import javafx.scene.chart.Axis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
@@ -42,53 +43,50 @@ import javafx.scene.shape.Rectangle;
 
 
 public class GraphPaneController {
-	
+
 	@FXML
 	private AnchorPane anchorPane;
-	
+
 	@FXML
 	private TabPane tabPane;
-	
+
 	@FXML
 	private AnchorPane runningPane;
-	
+
 	@FXML
 	private AnchorPane speedprofilePane;
-	
+
 	@FXML
 	private AnchorPane timeDistancePane;
-	
+
 	@FXML
 	private ListView<String> trainNumbers;
-	
+
 	@FXML
 	public void initialize() {
         tabPane.setSide(Side.BOTTOM);
-        
+
         timeDistanceChart = createCourseForTimeChart();
         speedProfileChart = createVelocityChart();
-        
+
         speedprofilePane.getChildren().add(speedProfileChart);
         timeDistancePane.getChildren().add(timeDistanceChart);
-        
-        
+
+
         AnchorPane.setTopAnchor(timeDistanceChart, 0.0);
         AnchorPane.setLeftAnchor(timeDistanceChart, 0.0);
         AnchorPane.setRightAnchor(timeDistanceChart, 0.0);
         AnchorPane.setBottomAnchor(timeDistanceChart, 0.0);
-        
+
         AnchorPane.setTopAnchor(speedProfileChart, 0.0);
         AnchorPane.setLeftAnchor(speedProfileChart, 0.0);
         AnchorPane.setRightAnchor(speedProfileChart, 0.0);
         AnchorPane.setBottomAnchor(speedProfileChart, 0.0);
-  
+
       new ZoomOnlyX(speedProfileChart, speedprofilePane);
       new Zoom(timeDistanceChart, timeDistancePane);
- 
-		Panning speedprofilePanning = new Panning(speedProfileChart);
-		Panning timeDistancePanning = new Panning(timeDistanceChart);
 
-		speedprofilePanning.setMouseFilter(new EventHandler<MouseEvent>() {
+      timeDistanceChart.setMouseFilter(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent mouseEvent) {
 				if (mouseEvent.getButton() == MouseButton.PRIMARY) {
@@ -96,25 +94,22 @@ public class GraphPaneController {
 					mouseEvent.consume();
 				}
 			}
-		});
-		speedprofilePanning.start();
+	  });
+      timeDistanceChart.startEventHandlers();
 
-		timeDistancePanning.setMouseFilter(new EventHandler<MouseEvent>() {
+      speedProfileChart.setMouseFilter(new EventHandler<MouseEvent>() {
 			@Override
 			public void handle(MouseEvent mouseEvent) {
 				if (mouseEvent.getButton() == MouseButton.PRIMARY) {
-
 				} else {
 					mouseEvent.consume();
 				}
 			}
-		});
-		timeDistancePanning.start();
-		
-
+	  });
+      speedProfileChart.startEventHandlers();
 	}
-	
-	
+
+
 	@FXML
 	private void resetSpeedProfile(MouseEvent event) {
 		if (event.getButton().equals(MouseButton.SECONDARY)) {
@@ -124,7 +119,7 @@ public class GraphPaneController {
 			}
 		}
 	}
-	
+
 	@FXML
 	private void resetTimeDistance(MouseEvent event) {
 		if (event.getButton().equals(MouseButton.SECONDARY)) {
@@ -135,17 +130,17 @@ public class GraphPaneController {
 		}
 	}
 
-	private LineChart<Number, Number> createVelocityChart() {
+	private DraggableChart<Number, Number> createVelocityChart() {
 		NumberAxis xAxis = createXAxis();
-	    NumberAxis yAxis = createYAxis();   
-	    LineChart<Number, Number> chart = new LineChart<>(xAxis, yAxis);
+	    NumberAxis yAxis = createYAxis();
+	    DraggableChart<Number, Number> chart = new DraggableChart<>(xAxis, yAxis);
 	    chart.setAnimated(false);
-	    chart.setCreateSymbols(false);	    
+	    chart.setCreateSymbols(false);
 	    trainNumbers.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) { 
-    			System.out.println(getTrain(newValue));		
-       		    drawVelocityTable(getTrain(newValue), chart); 
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+    			System.out.println(getTrain(newValue));
+       		    drawVelocityTable(getTrain(newValue), chart);
 
             }
         });
@@ -154,29 +149,28 @@ public class GraphPaneController {
 
 	private BlockingTimeChart<Number, Number> createCourseForTimeChart() {
 		NumberAxis xAxis = createXAxis2();
-	    NumberAxis yAxis = createYAxis2();  
+	    NumberAxis yAxis = createYAxis2();
 	    BlockingTimeChart<Number,Number> chart = new BlockingTimeChart<Number,Number>(xAxis,yAxis);
-	    
-	    trainNumbers.setOnMouseClicked(new EventHandler<MouseEvent>() {
-	    
+	    chart.getBlockingTimeChartPlotChildren();
 
+	    trainNumbers.setOnMouseClicked(new EventHandler<MouseEvent>() {
 	        @Override
 	        public void handle(MouseEvent event) {
 	            System.out.println("clicked on" + getTrain(trainNumbers.getSelectionModel().getSelectedItem().toString()));
 
 	        	List<BlockingTime> blockingTimes = getBlockingTimeStairway(getTrain(trainNumbers.getSelectionModel().getSelectedItem().toString()));
 	    		for (BlockingTime blockingTime : blockingTimes) {
-		            chart.addRectangle(blockingTime.getStartMeter(),blockingTime.getStartTimeInSecond(),
-		            		blockingTime.getEndMeter()-blockingTime.getStartMeter(), blockingTime.getEndTimeInSecond()-blockingTime.getStartTimeInSecond());
+	//	            chart.addRectangle(blockingTime.getStartMeter(),blockingTime.getStartTimeInSecond(),
+	//	            		blockingTime.getEndMeter()-blockingTime.getStartMeter(), blockingTime.getEndTimeInSecond()-blockingTime.getStartTimeInSecond());
 	    		}
-	    		
-	          
+
+
 	        }
 	    });
-	    
+
 	    trainNumbers.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override
-            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) { 
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
             	chart.removeRectangle();
                 chart.getData().clear();
     			drawCourseforTimeTable(getTrain(newValue), chart);
@@ -197,33 +191,33 @@ public class GraphPaneController {
         });
 	    xAxis.setSide(Side.TOP);
 	    return chart ;
-	}	
+	}
 
     private NumberAxis createXAxis() {
         NumberAxis xAxis = new NumberAxis();
         return xAxis;
     }
-    
+
     private NumberAxis createYAxis() {
     	NumberAxis yAxis = new NumberAxis();
 
         return yAxis;
     }
-    
+
     private NumberAxis createXAxis2() {
         NumberAxis xAxis = new NumberAxis();
         return xAxis;
     }
-    
+
     private NumberAxis createYAxis2() {
     	NumberAxis yAxis = new NumberAxis();
         return yAxis;
     }
-    
+
 	public void setTrainList(List<AbstractTrainSimulator> trainList) {
         this.updateTrainMap(trainList);
 	}
-	
+
 	public void updateTrainMap(List<AbstractTrainSimulator> trainList) {
 		CopyOnWriteArrayList<AbstractTrainSimulator> tempList = new
 			CopyOnWriteArrayList<AbstractTrainSimulator>();
@@ -237,17 +231,17 @@ public class GraphPaneController {
 			}
 		}
 		trainNumbers.setItems(numbers);
-		
+
 	}
-	
-	
+
+
 	public AbstractTrainSimulator getTrain(String trainNumber) {
 		return this.trainMap.get(trainNumber);
 	}
-	
+
 
 	public LineChart<Number, Number> drawVelocityTable(AbstractTrainSimulator train, LineChart<Number, Number> chart) {
-		chart.getData().clear(); 
+		chart.getData().clear();
 		XYChart.Series<Number, Number> CourseForVelocitySeries = new Series<Number, Number>();
 		CourseForVelocitySeries.setName("course for velocity");
 		if (train.getTrain().getStatus() != SimpleTrain.INACTIVE) {
@@ -256,10 +250,10 @@ public class GraphPaneController {
 		}
 		chart.getData().add(CourseForVelocitySeries);
 		XYChart.Series<Number, Number> speedLimitSeries = new Series<Number, Number>();
-		
+
 		speedLimitSeries.setName("speedlimit");
-		double y = -1;   
-		speedLimitSeries.getData().add(new Data<Number, Number>(0, y));   
+		double y = -1;
+		speedLimitSeries.getData().add(new Data<Number, Number>(0, y));
 	    for(Map.Entry<Double,Double> entry: getSpeedLimit(train).entrySet()) {
 	        if (y >= 0) {
 	            speedLimitSeries.getData().add(new Data<Number, Number>(entry.getKey(), y));
@@ -276,11 +270,11 @@ public class GraphPaneController {
 	}
 
 	public BlockingTimeChart<Number, Number> drawCourseforTimeTable(AbstractTrainSimulator train, BlockingTimeChart<Number, Number> chart) {
-		chart.getData().clear(); 
+		chart.getData().clear();
 		XYChart.Series<Number, Number> courseForTimeSeries = new Series<Number, Number>();
 		courseForTimeSeries.setName("course for time");
-		double y = -1;   
-		courseForTimeSeries.getData().add(new Data<Number, Number>(0, y));   
+		double y = -1;
+		courseForTimeSeries.getData().add(new Data<Number, Number>(0, y));
 		if (train.getTrain().getStatus() != SimpleTrain.INACTIVE) {
 			for(Map.Entry<Double,Double> entry : getCourseForTime(train).entrySet()) {
 				courseForTimeSeries.getData().add(new Data<Number, Number>(entry.getKey(), (entry.getValue()*-1)));
@@ -288,13 +282,13 @@ public class GraphPaneController {
 			chart.getData().add(courseForTimeSeries);
 			chart.setCreateSymbols(false);
 		}
-		
+
 		List<BlockingTime> blockingTimes = this.getBlockingTimeStairway(train);
 		for (BlockingTime blockingTime : blockingTimes) {
 			blockingTime.getEndMeter();
 		}
-		
-		return chart;	
+
+		return chart;
 	}
 
 	// Map: Meter, VelocityInKmH
@@ -309,13 +303,13 @@ public class GraphPaneController {
 		}
 		return velocityMap;
 	}
-	
+
 	// Map: Meter, TimeInSecond
 	private Map<Double, Double> getCourseForTime(AbstractTrainSimulator train) {
 		Map<Double, Double> timeMap = new LinkedHashMap<Double, Double>();
 		double meter = 0; // x
 		double timeInSecond = 0; // y
-		
+
 		for (DiscretePoint point : train.getWholeCoursePoints()) {
 			timeInSecond += point.getDuration().getTotalSecond();
 			meter += point.getDistance().getMeter();
@@ -323,40 +317,40 @@ public class GraphPaneController {
 		}
 		return timeMap;
 	}
-	
+
 	private LinkedHashMap<Double, Double> getSpeedLimit(AbstractTrainSimulator train) {
 		// Velocity
 		LinkedHashMap<Double, Double> speedLimitMap = new LinkedHashMap<Double, Double>();
-		
+
 		LinkPath path = train.getFullPath();
-		
+
 		double maxTrainKmH = train.getTrainDefinition().getMaxVelocity().getKilometerPerHour();
-		double maxKmH = Math.min(maxTrainKmH, 
+		double maxKmH = Math.min(maxTrainKmH,
 			path.getLinkEdges().get(0).getLink().getGeometry().getMaxVelocity().getKilometerPerHour());
 		double lastMeter = 0;
 		double meter = 0;
-		
+
 		for (LinkEdge edge : path.getLinkEdges()) {
 			if (edge.getLength().getMeter() == 0) {
 				continue;
 			}
-			
+
 			double linkKmH = edge.getLink().getGeometry().getMaxVelocity().getKilometerPerHour();
-			
+
 			if (Math.abs(Math.min(maxTrainKmH, linkKmH) - maxKmH) >= UnitUtility.ERROR) {
 				speedLimitMap.put(lastMeter, maxKmH);
 				speedLimitMap.put(meter, maxKmH);
-				
+
 				maxKmH = Math.min(maxTrainKmH, linkKmH);
 				lastMeter = meter;
 			}
-			
+
 			meter += edge.getLength().getMeter();
 		}
-		
+
 		speedLimitMap.put(lastMeter, maxKmH);
 		speedLimitMap.put(meter, maxKmH);
-		
+
 		return speedLimitMap;
 	}
 
@@ -370,31 +364,31 @@ public class GraphPaneController {
 				if (resource.getReleaseTime() == null) {
 					break;
 				}
-				
+
 				double startMeter = meter;
 				double endMeter = meter + resource.getPartialRoute().getPath().getLength().getMeter();
-				double startTimeInSecond = resource.getGrantTime().getDifference(trainStartTime).getTotalSecond();			
+				double startTimeInSecond = resource.getGrantTime().getDifference(trainStartTime).getTotalSecond();
 				double endTimeInSecond = resource.getReleaseTime().getDifference(trainStartTime).getTotalSecond();
-				
+
 				blockingTimes.add(new BlockingTime(startMeter, endMeter, startTimeInSecond, endTimeInSecond));
-				
+
 				meter = endMeter;
 			}
-			
+
 		}
-		
+
 		return blockingTimes;
 	}
-	
 
-	LineChart<Number, Number> timeDistanceChart;
-	LineChart<Number, Number> speedProfileChart;
-	
+
+	DraggableChart<Number, Number> timeDistanceChart;
+	DraggableChart<Number, Number> speedProfileChart;
+
 	ObservableList<String> numbers = FXCollections.observableArrayList();
 	private ConcurrentHashMap<String, AbstractTrainSimulator> trainMap =
 			new ConcurrentHashMap<String, AbstractTrainSimulator>();
-	
-	class BlockingTime {		
+
+	class BlockingTime {
 		BlockingTime(double startMeter, double endMeter,
 				double startTimeInSecond, double endTimeInSecond) {
 			super();
@@ -403,19 +397,19 @@ public class GraphPaneController {
 			this.startTimeInSecond = startTimeInSecond;
 			this.endTimeInSecond = endTimeInSecond;
 		}
-			
+
 		public double getStartMeter() {
 			return startMeter;
 		}
-		
+
 		public double getEndMeter() {
 			return endMeter;
 		}
-		
+
 		public double getStartTimeInSecond() {
 			return startTimeInSecond;
 		}
-		
+
 		public double getEndTimeInSecond() {
 			return endTimeInSecond;
 		}
@@ -425,23 +419,23 @@ public class GraphPaneController {
 		private double startTimeInSecond;
 		private double endTimeInSecond;
 	}
-	
-	class BlockingTimeChart<X,Y> extends LineChart<X,Y> {
-		
+
+	class BlockingTimeChart<X,Y> extends DraggableChart<X,Y> {
+
 
 		public BlockingTimeChart(Axis<X> xAxis, Axis<Y> yAxis) {
 			super(xAxis, yAxis);
 
-			
+
 			// TODO Auto-generated constructor stub
 		}
-		
+
 		void setBlockingTime(List<BlockingTime> blockingTimes) {
 			this.blockingTimes = blockingTimes;
 		}
-		
+
 		 public void addRectangle(double x, double y, double width, double height) {
-		        r = new Rectangle();	        
+		        r = new Rectangle();
 		        r.setX(this.getXAxis().getDisplayPosition(this.getXAxis().toRealValue(x)));
                 r.setY(this.getYAxis().getDisplayPosition(this.getYAxis().toRealValue(-y)));
                 r.setWidth(this.getXAxis().getDisplayPosition(this.getXAxis().toRealValue(width)));
@@ -449,39 +443,42 @@ public class GraphPaneController {
                 r.setFill(Color.BLUE.deriveColor(0, 1, 1, 0.5));
          		r.setMouseTransparent(true);
                 this.getPlotChildren().add(r);
-                
+
 		    }
-		 
+
 		 public void removeRectangle () {
 			 this.getPlotChildren().clear();
 		 }
 
-		
+		public ObservableList<Node> getBlockingTimeChartPlotChildren() {
+			return this.getPlotChildren();
+		}
+
+
 		 @Override
         protected void layoutPlotChildren() {
             super.layoutPlotChildren();
- /**           if (this.blockingTimes != null) {
+            if (this.blockingTimes != null) {
             	int i = 0;
             	for (BlockingTime blockingTime : this.blockingTimes) {
-            		 Rectangle r = new Rectangle();
+            		 r = new Rectangle();
  //           		 X startX = this.getXAxis().toRealValue(10);
  //           		 Y startY = this.getYAxis().toRealValue(10);
-                     r.setX(10 + i * 200);
-                     r.setY(10 + i * 100);
-                     r.setWidth(200);
-                     r.setHeight(100);
+                     r.setX(this.getXAxis().getDisplayPosition(this.getXAxis().toRealValue(blockingTime.getStartMeter())));
+                     r.setY(this.getYAxis().getDisplayPosition(this.getYAxis().toRealValue(blockingTime.getStartTimeInSecond())));
+                     r.setWidth(this.getXAxis().getDisplayPosition(this.getXAxis().toRealValue(blockingTime.getEndMeter()-blockingTime.getStartMeter())));
+                     r.setHeight(this.getYAxis().getDisplayPosition(this.getYAxis().toRealValue(-(blockingTime.getEndTimeInSecond()-blockingTime.getStartTimeInSecond()))));
                     r.setFill(Color.GREEN.deriveColor(0, 1, 1, 0.5));
-             		r.setVisible(false);
              		r.setMouseTransparent(true);
                      this.getPlotChildren().add(r);
                      i++;
             	}
-            	 
+
             }
         }
-**/}		 
+
 		private List<BlockingTime> blockingTimes;
 		Rectangle r;
 	}
-	
+
 }
