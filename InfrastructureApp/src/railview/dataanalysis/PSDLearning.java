5e5ba9122c7ca100c05820b374f7d2f5a0b69b79
herrcui <dr.yong.cui@gmail.com>
2017-04-28 13:21:27 +0200

    * file path of csv data

--- InfrastructureApp/src/railview/dataanalysis/PSDLearning.java
+++ InfrastructureApp/src/railview/dataanalysis/PSDLearning.java
@@ -11,22 +11,22 @@ import ml.supervise.TrainingDataReader;
 public class PSDLearning {
 	public static void main(String[] args) {
 		try {
-			Path path = Paths.get("C:\\Users\\cu.IEV-VWI962\\Downloads\\Lat_L_V_0_con.csv");
-			
+			Path path = Paths.get("C:\\temp\\Lat_R_V_1212_con.csv");
+
 			List<TrainingData> trainingData = TrainingDataReader.readTrainingData(path);
-			
+
 			SupervisedLearning SL = new SupervisedLearning(trainingData,
 					1,
 					2,
-					new double[] {6.276413371708169, 3.5712782411506234, 1.7687694577853579, 14.556417290067378});
+					new double[] {0.05262765218052027, 0.07783881658756395, 0.3489742126034168, 0.348953243164028});
 					//new double[] {0, 0, 0, 0});
-			
-			SL.setMaxRound(100000);
-			
+
+			SL.setMaxRound(1000000);
+
 			SL.learn(0.1);
-			
+
 			String s = "";
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
