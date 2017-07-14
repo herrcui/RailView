b4a7cc6985034f21be474c94bd939ef0b6e086c8
Katsching <kai.chen@gmx.de>
2017-07-14 03:37:10 +0200

    * added some parts for displaying signals

--- RailViewLibrary/src/railview/infrastructure/container/InfrastructureElementsPane.java
+++ RailViewLibrary/src/railview/infrastructure/container/InfrastructureElementsPane.java
@@ -22,10 +22,9 @@ public class InfrastructureElementsPane extends PannablePane {
 	private CoordinateMapper mapper;
 	private Color elementColor;
 	private Color signalColor;
-	
 	private List<Coordinate> path = null;
 	private Coordinate eventPoint = null;
-	
+
 	private double elementWidth = 0.1;
 
 	public InfrastructureElementsPane() {
@@ -36,31 +35,32 @@ public class InfrastructureElementsPane extends PannablePane {
 	public void setCoordinateMapper(CoordinateMapper mapper) {
 		this.mapper = mapper;
 	}
-	
+
 	public void setHighlightedPath(List<Coordinate> path) {
 		this.path = path;
 	}
-	
+
 	public void setEventPoint(Coordinate coordinate) {
 		this.eventPoint = coordinate;
 	}
 
-	public void setAndDrawElements(Collection<InfrastructureElement> elements, Color elementColor) {
+	public void setAndDrawElements(Collection<InfrastructureElement> elements,
+			Color elementColor) {
 		this.elements = elements;
 		this.elementColor = elementColor;
 		this.draw();
 	}
-	
+
 	public void setSignals(Collection<AbstractSignal> signals, Color signalColor) {
 		// TODO Auto-generated method stub
 		this.signals = signals;
 		this.signalColor = signalColor;
 	}
-	
+
 	public ObservableList<Node> getChrildren() {
 		return this.getChrildren();
 	}
-	
+
 	public void setElements(Collection<InfrastructureElement> elements) {
 		this.elements = elements;
 	}
@@ -74,13 +74,14 @@ public class InfrastructureElementsPane extends PannablePane {
 		for (InfrastructureElement element : this.elements) {
 			this.drawInfrastructureElement(element);
 		}
-		
-		if (this.signals != null) { // for some panes, it is not necessary to draw signals.
+
+		if (this.signals != null) { // for some panes, it is not necessary to
+									// draw signals.
 			for (AbstractSignal signal : this.signals) {
 				this.drawSignal(signal);
 			}
 		}
-		
+
 		this.drawPath();
 		this.drawEventPoint();
 	}
@@ -112,7 +113,8 @@ public class InfrastructureElementsPane extends PannablePane {
 		}
 	}
 
-	private void drawLink(List<Coordinate> coordinates, Color color, double width) {
+	private void drawLink(List<Coordinate> coordinates, Color color,
+			double width) {
 		for (int i = 0; i < coordinates.size() - 1; i++) {
 			Line line = new Line();
 
@@ -127,42 +129,213 @@ public class InfrastructureElementsPane extends PannablePane {
 			this.getChildren().add(line);
 		}
 	}
-	
+
 	private void drawSignal(AbstractSignal signal) {
 		RelativePosition position = signal.getPositions().get(0);
-		double percentage = position.getDistance().getMeter()/position.getLink().getGeometry().getLength().getMeter();
-		
+		double percentage = position.getDistance().getMeter()
+				/ position.getLink().getGeometry().getLength().getMeter();
+
 		List<Coordinate> coordinates = position.getLink().getCoordinates();
 		List<Double> coorDists = new ArrayList<Double>();
 		double totalCoorDist = 0;
 		for (int i = 0; i < coordinates.size() - 1; i++) {
-			double coorDist = Coordinate.getDistance(coordinates.get(i), coordinates.get(i+1));
+			double coorDist = Coordinate.getDistance(coordinates.get(i),
+					coordinates.get(i + 1));
 			totalCoorDist += coorDist;
 			coorDists.add(totalCoorDist);
 		}
-		
+
 		InfrastructureObjectCoordinate signalCoor = null;
 		for (int i = 0; i < coordinates.size() - 1; i++) {
-			if (coorDists.get(i)/totalCoorDist >= percentage) {
-				double percentageInSegment = (coorDists.get(i)/totalCoorDist - percentage) * totalCoorDist /
-						(i==0 ? coorDists.get(i) : (coorDists.get(i) - coorDists.get(i-1)));
+			if (coorDists.get(i) / totalCoorDist >= percentage) {
+				double percentageInSegment = (coorDists.get(i) / totalCoorDist - percentage)
+						* totalCoorDist
+						/ (i == 0 ? coorDists.get(i)
+								: (coorDists.get(i) - coorDists.get(i - 1)));
 				signalCoor = new InfrastructureObjectCoordinate(
-					coordinates.get(i),
-					coordinates.get(i+1),
-					percentageInSegment);
+						coordinates.get(i), coordinates.get(i + 1),
+						percentageInSegment);
 				break;
 			}
 		}
 		if (signalCoor == null) {
 			signalCoor = new InfrastructureObjectCoordinate(
-				coordinates.get(coordinates.size() - 2),
-				coordinates.get(coordinates.size() - 1),
-				1);
+					coordinates.get(coordinates.size() - 2),
+					coordinates.get(coordinates.size() - 1), 1);
 		}
-		
+
+		double xStart = signalCoor.getStart().getX();
+		double yStart = signalCoor.getStart().getY();
+		double xEnd = signalCoor.getEnd().getX();
+		double yEnd = signalCoor.getEnd().getY();
+
+		// random static number a and b
+		double a = 450;
+		double b = 150;
+
+		double xCoordinateP0;
+		double yCoordinateP0;
+		double xCoordinateP3;
+		double yCoordinateP3;
+
+		double signalStartX = xStart + (percentage * (xEnd - xStart));
+		double signalStartY = yStart + (percentage * (yEnd - yStart));
+
+		// angle between line and x-axis
+//		double angle = (yEnd - yStart) / (xEnd - xStart);
+		double alpha = Math.atan2(yEnd - yStart, xEnd - xStart);
+
+		// parallel line
+		Line line = new Line();
+		// orthogonal line
+		Line orthLine = new Line();
+		Circle circle = new Circle();
+
+		// line is horizontal
+		if (yStart == yEnd) {
+			line.setStartX(mapper.mapToPaneX(signalStartX, this));
+			line.setStartY(mapper.mapToPaneY(signalStartY + a, this));
+			line.setEndX(mapper.mapToPaneX(signalStartX + a, this));
+			line.setEndY(mapper.mapToPaneY(signalStartY + a, this));
+			line.setStroke(Color.RED);
+			line.setStrokeWidth(0.1);
+
+			orthLine.setStartX(mapper.mapToPaneX(signalStartX, this));
+			orthLine.setStartY(mapper.mapToPaneY(signalStartY + a + (b), this));
+			orthLine.setEndX(mapper.mapToPaneX(signalStartX, this));
+			orthLine.setEndY(mapper.mapToPaneY(signalStartY + a - (b), this));
+			orthLine.setStroke(Color.RED);
+			orthLine.setStrokeWidth(0.1);
+			circle.setRadius(0.000000000000000001);
+			circle.setCenterX(mapper.mapToPaneX(signalStartX + a + circle.getRadius(), this));
+			circle.setCenterY(mapper.mapToPaneY(signalStartY + a + circle.getRadius(), this));
+			circle.setStroke(Color.RED);
+		}
+
+		// line is vertical
+		else if (xStart == xEnd) {
+			line.setStartX(mapper.mapToPaneX(signalStartX + a, this));
+			line.setStartY(mapper.mapToPaneY(signalStartY, this));
+			line.setEndX(mapper.mapToPaneX(signalStartX + a, this));
+			line.setEndY(mapper.mapToPaneY(signalStartY + a, this));
+			line.setStroke(Color.RED);
+			line.setStrokeWidth(0.1);
+
+			orthLine.setStartX(mapper.mapToPaneX(signalStartX + a + (b), this));
+			orthLine.setStartY(mapper.mapToPaneY(signalStartY, this));
+			orthLine.setEndX(mapper.mapToPaneX(signalStartX + a - (b), this));
+			orthLine.setEndY(mapper.mapToPaneY(signalStartY, this));
+			orthLine.setStroke(Color.RED);
+			orthLine.setStrokeWidth(0.1);
+
+			circle.setRadius(0.000000000000000001);
+			circle.setCenterX(mapper.mapToPaneX(signalStartX + a + circle.getRadius(), this));
+			circle.setCenterY(mapper.mapToPaneY(signalStartY + a + circle.getRadius(), this));
+			circle.setStrokeWidth(0.2);
+			circle.setStroke(Color.RED);
+		}
+
+		// 0 - 360°
+		else if (xEnd > xStart && yEnd > yStart) {
+			xCoordinateP0 = signalStartX - (a * (Math.sin(alpha)));
+			yCoordinateP0 = signalStartY + (a * (Math.cos(alpha)));
+			xCoordinateP3 = signalStartX - (a - b / 2) * (Math.sin(alpha));
+			yCoordinateP3 = signalStartY + (a - b / 2) * (Math.cos(alpha));
+			line.setStartX(mapper.mapToPaneX(xCoordinateP0, this));
+			line.setStartY(mapper.mapToPaneY(yCoordinateP0, this));
+			line.setEndX(mapper.mapToPaneX(
+					xCoordinateP0 + (a * (Math.cos(alpha))), this));
+			line.setEndY(mapper.mapToPaneY(
+					yCoordinateP0 + (a * (Math.sin(alpha))), this));
+			line.setStroke(Color.RED);
+			circle.setStrokeWidth(0.1);
+			line.setStrokeWidth(0.1);
+
+			orthLine.setStartX(mapper.mapToPaneX(xCoordinateP3, this));
+			orthLine.setStartY(mapper.mapToPaneY(yCoordinateP3, this));
+			orthLine.setEndX(mapper.mapToPaneX(xCoordinateP0, this));
+			orthLine.setEndY(mapper.mapToPaneY(yCoordinateP0, this));
+			orthLine.setStroke(Color.RED);
+			orthLine.setStrokeWidth(0.1);
+
+		}
+
+		else if (xEnd > xStart && yEnd < yStart) {
+			xCoordinateP0 = signalStartX + (a * (Math.sin(alpha)));
+			yCoordinateP0 = signalStartY + (a * (Math.cos(alpha)));
+			xCoordinateP3 = signalStartX + (a - b / 2) * (Math.sin(alpha));
+			yCoordinateP3 = signalStartY + (a - b / 2) * (Math.cos(alpha));
+			line.setStartX(mapper.mapToPaneX(xCoordinateP0, this));
+			line.setStartY(mapper.mapToPaneY(yCoordinateP0, this));
+			line.setEndX(mapper.mapToPaneX(
+					xCoordinateP0 + (a * (Math.cos(alpha))), this));
+			line.setEndY(mapper.mapToPaneY(
+					yCoordinateP0 - (a * (Math.sin(alpha))), this));
+			line.setStroke(Color.RED);
+			circle.setStrokeWidth(0.1);
+			line.setStrokeWidth(0.1);
+
+			orthLine.setStartX(mapper.mapToPaneX(xCoordinateP3, this));
+			orthLine.setStartY(mapper.mapToPaneY(yCoordinateP3, this));
+			orthLine.setEndX(mapper.mapToPaneX(xCoordinateP0, this));
+			orthLine.setEndY(mapper.mapToPaneY(yCoordinateP0, this));
+			orthLine.setStroke(Color.RED);
+			orthLine.setStrokeWidth(0.1);
+		}
+
+		else if (xEnd < xStart && yEnd < yStart) {
+			xCoordinateP0 = signalStartX + (a * (Math.sin(alpha)));
+			yCoordinateP0 = signalStartY - (a * (Math.cos(alpha)));
+			xCoordinateP3 = signalStartX + (a - b / 2) * (Math.sin(alpha));
+			yCoordinateP3 = signalStartY - (a - b / 2) * (Math.cos(alpha));
+			line.setStartX(mapper.mapToPaneX(xCoordinateP0, this));
+			line.setStartY(mapper.mapToPaneY(yCoordinateP0, this));
+			line.setEndX(mapper.mapToPaneX(
+					xCoordinateP0 - (a * (Math.cos(alpha))), this));
+			line.setEndY(mapper.mapToPaneY(
+					yCoordinateP0 - (a * (Math.sin(alpha))), this));
+			line.setStroke(Color.RED);
+			circle.setStrokeWidth(0.1);
+			line.setStrokeWidth(0.1);
+
+			orthLine.setStartX(mapper.mapToPaneX(xCoordinateP3, this));
+			orthLine.setStartY(mapper.mapToPaneY(yCoordinateP3, this));
+			orthLine.setEndX(mapper.mapToPaneX(xCoordinateP0, this));
+			orthLine.setEndY(mapper.mapToPaneY(yCoordinateP0, this));
+			orthLine.setStroke(Color.RED);
+			orthLine.setStrokeWidth(0.1);
+		}
+
+		else if (xEnd < xStart && yEnd > yStart) {
+			xCoordinateP0 = signalStartX - (a * (Math.sin(alpha)));
+			yCoordinateP0 = signalStartY - (a * (Math.cos(alpha)));
+			xCoordinateP3 = signalStartX - (a - b / 2) * (Math.sin(alpha));
+			yCoordinateP3 = signalStartY - (a - b / 2) * (Math.cos(alpha));
+			xCoordinateP3 = signalStartX;
+			yCoordinateP3 = signalStartY;
+			line.setStartX(mapper.mapToPaneX(xCoordinateP0, this));
+			line.setStartY(mapper.mapToPaneY(yCoordinateP0, this));
+			line.setEndX(mapper.mapToPaneX(
+					xCoordinateP0 - (a * (Math.cos(alpha))), this));
+			line.setEndY(mapper.mapToPaneY(
+					yCoordinateP0 + (a * (Math.sin(alpha))), this));
+			line.setStroke(Color.RED);
+			circle.setStrokeWidth(0.1);
+			line.setStrokeWidth(0.1);
+
+			orthLine.setStartX(mapper.mapToPaneX(xCoordinateP3, this));
+			orthLine.setStartY(mapper.mapToPaneY(yCoordinateP3, this));
+			orthLine.setEndX(mapper.mapToPaneX(xCoordinateP0, this));
+			orthLine.setEndY(mapper.mapToPaneY(yCoordinateP0, this));
+			orthLine.setStroke(Color.RED);
+			orthLine.setStrokeWidth(0.1);
+		}
+
+		this.getChildren().addAll(line, orthLine);
+//		this.getChildren().add(circle);
 		System.out.println(signalCoor);
 	}
-	
+
 	private void drawPath() {
 		if (path == null) {
 			return;
@@ -170,28 +343,27 @@ public class InfrastructureElementsPane extends PannablePane {
 			drawLink(path, Color.CYAN, 0.4);
 		}
 	}
-	
+
 	private void drawEventPoint() {
 		if (this.eventPoint != null) {
 			Circle circle = new Circle();
-			
+
 			circle.setCenterX((mapper.mapToPaneX(eventPoint.getX(), this)));
 			circle.setCenterY((mapper.mapToPaneY(eventPoint.getY(), this)));
 			circle.setFill(Color.RED);
 			circle.setRadius(0.6);
-			
+
 			this.getChildren().add(circle);
 		}
 	}
-	
+
 	class InfrastructureObjectCoordinate {
 		private Coordinate start;
 		private Coordinate end;
 		private double percentage;
-		
+
 		public InfrastructureObjectCoordinate(railapp.units.Coordinate start,
-				railapp.units.Coordinate end,
-				double percentage) {
+				railapp.units.Coordinate end, double percentage) {
 			super();
 			this.start = start;
 			this.end = end;
@@ -205,7 +377,7 @@ public class InfrastructureElementsPane extends PannablePane {
 		public Coordinate getEnd() {
 			return end;
 		}
-		
+
 		public double percentage() {
 			return percentage;
 		}
